e521d44bc2e79e0b95aa3f5981c127646aa22e94
Maulana <mprambadi@gmail.com>
2020-12-11 18:43:41 +0700

    fix: image name

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ docker network rm live-discussion-app
 docker network create live-discussion-app
 
 docker run --name app-node -p 4000:4000 -d --network live-discussion-app mprambadi/app-node  
-docker run --name app-go -p 4001:4001 -d --network live-discussion-app username/app-go  
-docker run --name app-php -p 4002:4002 -d --network live-discussion-app username/app-php  
+docker run --name app-go -p 4001:4001 -d --network live-discussion-app mprambadi/app-go  
+docker run --name app-php -p 4002:4002 -d --network live-discussion-app mprambadi/app-php  
 docker run --name app-nginx -p 80:80 -d --network live-discussion-app -v $(pwd)/app-nginx/default.conf:/etc/nginx/nginx.conf nginx
