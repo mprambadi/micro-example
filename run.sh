b6f06b2a489e5d2f61c993155a541873bc14e409
Maulana <mprambadi@gmail.com>
2020-12-11 18:42:04 +0700

    feat: run on background

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ docker network rm live-discussion-app
 
 docker network create live-discussion-app
 
-docker run --name app-node -p 4000:4000 --network live-discussion-app mprambadi/app-node  
-docker run --name app-go -p 4001:4001 --network live-discussion-app username/app-go  
-docker run --name app-php -p 4002:4002 --network live-discussion-app username/app-php  
-docker run --name app-nginx -p 80:80 --network live-discussion-app -v $(pwd)/app-nginx/default.conf:/etc/nginx/nginx.conf nginx
+docker run --name app-node -p 4000:4000 -d --network live-discussion-app mprambadi/app-node  
+docker run --name app-go -p 4001:4001 -d --network live-discussion-app username/app-go  
+docker run --name app-php -p 4002:4002 -d --network live-discussion-app username/app-php  
+docker run --name app-nginx -p 80:80 -d --network live-discussion-app -v $(pwd)/app-nginx/default.conf:/etc/nginx/nginx.conf nginx
