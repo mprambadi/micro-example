1f06b4eafb889e8ef1a10a7885485b8ec5cc1b9c
Maulana <mprambadi@gmail.com>
2020-12-11 18:39:11 +0700

    feat: add shell

---
+++ run.sh
@@ -0,0 +1,9 @@
+docker rm -f $(docker ps -aq)
+docker network rm live-discussion-app
+
+docker network create live-discussion-app
+
+docker run --name app-node -p 4000:4000 --network live-discussion-app mprambadi/app-node  
+docker run --name app-go -p 4001:4001 --network live-discussion-app username/app-go  
+docker run --name app-php -p 4002:4002 --network live-discussion-app username/app-php  
+docker run --name app-nginx -p 80:80 --network live-discussion-app -v $(pwd)/app-nginx/default.conf:/etc/nginx/nginx.conf nginx
